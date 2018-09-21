880344f4fc2335935621d3d01e08256a5ce1ca1b
Tom Thomas <germantechie@users.noreply.github.com>
2018-09-21 09:18:47 +0530

    TAC XML for Wait less than 5sec
    Update Dynamic Wait Statement less than 5sec within TAC XML using Python ElementTree and reading through multiple files within nested subfolders too.
    This program however changes the namespaces that were already present in the script but opens up in TAC.

--- TACxmlRemWait.py
+++ TACxmlRemWait.py
@@ -6,7 +6,7 @@ from os import walk
 # > This script is written to parse the TAC xml scripts and find and replace those instances where dynamic Wait statement has the time set to less than 5000 ms.
 # > It is replaced to 5000ms so that performance of the script can be made better.
 
-def parseTACxml(inputFile):
+def parseTACxml(DirNAME, inputFile):
 	"This function is used to take a filename, parse for WaitTime less than 5000ms and change to 5000ms"
 	tree = ET.parse(inputFile)  
 
@@ -18,13 +18,22 @@ def parseTACxml(inputFile):
 			WaitTime = int(WaitInt.find('WaitTime').text)			  # converting the time value text to int
 			if(WaitTime < 5000):  									  # Work upon those values whose time is less than 5000ms
 				cnt = cnt + 1
-				print(WaitInt.attrib, ":", whichWaitStatement, ":", WaitTime)
-
+				WaitInt.find('WaitTime').text = str(5000)
+				print(WaitInt.attrib, ":", whichWaitStatement, ":", WaitTime, ":", WaitInt.find('WaitTime').text)
+				
 
 	print("Total to be rectified:",cnt)
+	outPutFile = DirNAME + "output.xml"
+	tree.write(outPutFile)
 
 # -- -- Main Program is here ---
-FullFilePath = 'C:\\Tom_DataFolder\\AT081_TAC_Changed.xml'
-parseTACxml(FullFilePath)
+folderPath = 'C:\\1_Automation_Tools\\3_Python\\2_Page_Manuscript_Parsing\\TAC\\'
+# get all filenames within the given folder into list
+for (dirpath, dirnames, filenames) in walk(folderPath):
+	for name in filenames:
+		if name.find(".xml") > 0:						 # Work only with xml files in a directory
+			FullFilePath = os.path.join(dirpath, name)   # Join the path with filename
+			print(dirpath)
+			parseTACxml(dirpath, FullFilePath)
 	
 print("------------ All DONE ------------------")	
